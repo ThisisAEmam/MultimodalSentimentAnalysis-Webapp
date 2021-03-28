d112c3e1b7f45013e87c86d1cde1b4e90e56de6d
Swilam20 <mahmoud.swilam.2020@gmail.com>
2021-03-28 08:33:20 +0200

    27/3/2021

--- Front-end/src/pages/GettingStarted/GettingStarted.js
+++ Front-end/src/pages/GettingStarted/GettingStarted.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Link } from "react-router-dom";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import classes from "./GettingStarted.module.css";
 import "./transitions.css";
 
 const GettingStarted = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [showBack, setShowBack] = useState(false);
+  const [nextToDone, setNextToDone] = useState(false);
+
+  useEffect(() => {
+    if (currentIndex > 0) {
+      setShowBack(true);
+    } else {
+      setShowBack(false);
+    }
+    if (currentIndex >= 4) {
+      setNextToDone(true);
+    } else {
+      setNextToDone(false);
+    }
+  }, [currentIndex]);
 
   const nextClickHandler = () => {
     setCurrentIndex(currentIndex + 1);
   };
 
-  const prevClickHandler = () => {
+  const backClickHandler = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  const doneClickHandler = () => {
     setCurrentIndex(currentIndex - 1);
   };
 
@@ -22,12 +42,24 @@ const GettingStarted = (props) => {
         <div className={classes.body}>
           <TransitionGroup>
             <CSSTransition key={currentIndex} timeout={1000} classNames={"slide"}>
-              <div className={classes.slideItem}>{currentIndex}</div>
+              <div className={classes.slideItem}></div>
             </CSSTransition>
           </TransitionGroup>
         </div>
-        <button onClick={prevClickHandler}>prev</button>
-        <button onClick={nextClickHandler}>next</button>
+        <div className={classes.btnsContainer}>
+          <button className={[classes.backBtn, showBack ? classes.showBtn : null].join(" ")} onClick={backClickHandler}>
+            Back
+          </button>
+          {!nextToDone ? (
+            <button className={classes.nextBtn} onClick={nextClickHandler}>
+              Next
+            </button>
+          ) : (
+            <button className={classes.nextBtn} onClick={doneClickHandler}>
+              <Link to="/dashboard/overview">Done</Link>
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
