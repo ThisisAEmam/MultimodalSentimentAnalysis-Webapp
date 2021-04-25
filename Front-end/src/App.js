442212d6650d671b4fa1450f34823126f425b204
ThisisAEmam <abdoemamofficial@gmail.com>
2021-04-25 14:57:14 +0200

    25/4/2021

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -5,9 +5,22 @@ import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Game from "./pages/Game/Game";
+import AboutUS from "./pages/AboutUS/AboutUS";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
-function App() {
+const App = () => {
   const screenDispatch = useDispatch(setScreen);
+
+  window.addEventListener("resize", () => {
+    if (window.outerWidth < 1000) {
+      screenDispatch(setScreen("Mobile"));
+    } else if (window.outerWidth < 1367 && window.outerWidth > 1000) {
+      screenDispatch(setScreen("HD"));
+    } else if (window.outerWidth > 1367) {
+      screenDispatch(setScreen("Full HD"));
+    }
+  });
 
   useEffect(() => {
     if (window.outerWidth < 1000) {
@@ -26,10 +39,13 @@ function App() {
         <Switch>
           <Route path="/" exact component={Homepage} />
           <Route path="/dashboard" component={Dashboard} />
+          <Route path="/game" exact component={Game} />
+          <Route path="/about" exact component={AboutUS} />
+          <Route path="/login" exact component={LoginPage} />
         </Switch>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
