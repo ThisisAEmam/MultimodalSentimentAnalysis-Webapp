6274edb967b3f02c13ab2a166d5500b216ed3243
Swilam20 <mahmoud.swilam.2020@gmail.com>
2021-05-06 21:34:57 +0200

    06/05/2021

--- Front-end/src/pages/Dashboard/Newsletter/Newsletter.js
+++ Front-end/src/pages/Dashboard/Newsletter/Newsletter.js
@@ -1,55 +1,67 @@
-import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Notification from "../../../components/Dashboard/Notification/Notification";
 import PageTitle from "../../../components/Dashboard/PageTitle/PageTitle";
 import classes from "./Newsletter.module.css";
+import axios from "axios";
 
 const Newsletter = (props) => {
-  const [emptyName, setEmptyName] = useState(false);
-  const [emptyEmail, setEmptyEmail] = useState(false);
-  const nameRef = useRef();
-  const emailRef = useRef();
+  const [subscribed, setSubscribed] = useState(false);
   const [notification, setNotification] = useState(false);
   const [notificationMsg, setNotificationMsg] = useState("");
 
   const { loggedin } = useSelector((state) => state);
 
-  const clickHandler = () => {
-    const name = nameRef.current.value;
-    const email = emailRef.current.value;
+  const config = {
+    headers: {
+      Authorization: loggedin.token,
+    },
+  };
 
-    setEmptyName(false);
-    setEmptyEmail(false);
-    setNotification(false);
-
-    if (name.trim().length === 0 || email.trim().length === 0) {
-      if (name.trim().length === 0) {
-        setEmptyName(true);
-      }
-      if (email.trim().length === 0) {
-        setEmptyEmail(true);
-      }
-      return;
-    }
-    const config = {
-      headers: {
-        Authorization: loggedin.token,
-      },
-    };
+  useEffect(() => {
     axios
-      .post("/dashboard/newsletter", { name: name, email: email }, config)
+      .get("/dashboard/newsletter/isSubscribed", config)
       .then((res) => {
         if (res.data.success) {
-          setNotificationMsg(res.data.msg);
+          setSubscribed(res.data.data);
         } else {
-          setNotificationMsg(res.data.msg);
+          console.log(res.data.msg);
         }
       })
-      .catch((err) => console.log(err));
+      .catch((error) => console.log(error));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const clickHandler = () => {
+    if (subscribed) {
+      axios
+        .get("/dashboard/newsletter/unsubscribe", config)
+        .then((res) => {
+          if (res.data.success) {
+            setNotificationMsg(res.data.msg);
+            setSubscribed(false);
+          } else {
+            setNotificationMsg(res.data.msg);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      axios
+        .get("/dashboard/newsletter/subscribe", config)
+        .then((res) => {
+          if (res.data.success) {
+            setNotificationMsg(res.data.msg);
+            setSubscribed(true);
+          } else {
+            setNotificationMsg(res.data.msg);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
     setNotification(true);
-    nameRef.current.value = "";
-    emailRef.current.value = "";
+    setTimeout(() => {
+      setNotification(false);
+    }, 4000);
   };
 
   return (
@@ -58,17 +70,16 @@ const Newsletter = (props) => {
       <div className={classes.body}>
         <div className={classes.leftSide}>
           <p className={classes.text}>Subscribe to our newsletter to get the latest updates on our offers, features and new models we provide.</p>
-          <div className={classes.form}>
-            <label htmlFor="name">
-              Name <span>*</span>
-            </label>
-            <input name="name" ref={nameRef} className={emptyName ? classes.emptyInput : null} type="text" placeholder="Your name" />
-            <label htmlFor="email">
-              Email <span>*</span>
-            </label>
-            <input name="email" ref={emailRef} className={emptyEmail ? classes.emptyInput : null} type="email" placeholder="Your Email" />
-            <button onClick={clickHandler}>Submit</button>
-          </div>
+          {subscribed ? (
+            <button className={[classes.submitBtn, classes.subscribed].join(" ")} onClick={clickHandler}>
+              Unsubscribe
+            </button>
+          ) : (
+            <button className={classes.submitBtn} onClick={clickHandler}>
+              Subscribe
+            </button>
+          )}
+          {/* {subscribed ? <p className={classes.text2}>Note: You are already subscribed to our newsletter.</p> : null} */}
         </div>
         <img src="/newsletter.svg" alt="newsletter" className={classes.newsImg} />
       </div>
