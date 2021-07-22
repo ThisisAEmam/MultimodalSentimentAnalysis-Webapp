23f0964f3793078b85ca42fd1cafd00fbae4d9b6
ThisisAEmam <abdoemamofficial@gmail.com>
2021-07-22 13:14:26 +0200

    22/07/2021

--- Front-end/src/containers/Aboutpage/TeamMembers/data.js
+++ Front-end/src/containers/Aboutpage/TeamMembers/data.js
@@ -2,10 +2,11 @@ const data = [
   {
     name: "AbdelRahman Mohamed Emam",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
+      "Helped in creating the front-end of the web application using ReactJS",
+      "Helped in creating the back-end of the web application using NodeJS and Flask",
       "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in features extraction for the models' archetictures",
+      "Handled the server setup & preparation and domain DNS records setup using nginx",
     ],
     image: "emam.jpg",
     qoute:
@@ -14,10 +15,11 @@ const data = [
   {
     name: "Abdullah Ahmed Farhana",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
       "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in implementing the RAVEN model archeticture",
+      "Helped in feature extraction for the MulT model archeticture",
+      "Helped in writing the project documentation",
+      "Helped in creating the presentation's slideshow",
     ],
     image: "farhana.jpg",
     qoute:
@@ -26,24 +28,26 @@ const data = [
   {
     name: "Amr Mohamed Ezzat",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
       "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Implemented the Dataset Creator Tool functionalities",
+      "Implementing the video-to-utterances splitting script",
+      "Helped in feature extraction for the MulT model archeticture",
+      "Helped in writing the project documentation",
     ],
-    image: "emam.jpg",
+    image: "hamza.jpg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
   {
     name: "AbdelRahman Kadry Anwar",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
+      "Helped in implementing the RAVEN model archeticture",
       "Helped in implementing the MulT model archeticture",
       "Helped in feature extraction for the models' archetictures",
+      "Helped in writing the project documentation",
+      "Helped in creating the presentation's slideshow",
     ],
-    image: "emam.jpg",
+    image: "noimage.jpg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
@@ -51,95 +55,93 @@ const data = [
     name: "Mahmoud Swilam",
     responsibilities: [
       "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in writing the project (MARNN architecture) documentation",
+      "Created the Dataset Creator Tool GUI",
+      "Helped in creating the presentation's slideshow",
     ],
     image: "swilam.jpg",
-    qoute:
-      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
+    qoute: "The future belongs to the risk takers not the comfort seekers.",
   },
   {
     name: "Reem Nasser",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in implementing the RAVEN model archeticture",
+      "Helped in feature extraction for the RAVEN model archeticture",
+      "Helped in writing the project (RAVEN architecture) documentation",
+      "Helped in creating the presentation's slideshow",
     ],
-    image: "emam.jpg",
+    image: "noimage.jpg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
   {
     name: "Ali Alam-ElDin",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
+      "Helped in implementing the MARNN & MMUUBA models' archetictures",
       "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in writing the project (Back-end) documentation",
+      "Helped in creating the presentation's slideshow",
     ],
-    image: "emam.jpg",
+    image: "ali.jpeg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
   {
     name: "Mohamed El-Sayed",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in implementing the MARNN & MMUUBA models' archetictures",
+      "Helped in feature extraction for the MARNN & MMUUBA models' archetictures",
+      "Helped in writing the project (MARNN & MMUUBA) documentation",
+      "Helped in creating the presentation's slideshow",
     ],
     image: "mohamedElSayed.jpeg",
-    qoute:
-      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
+    qoute: "I hope this project help us now and help others later.",
   },
   {
     name: "Andrew Raafat",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in implementing the RAVEN model archeticture",
+      "Helped in feature extraction for the RAVEN model archeticture",
+      "Helped in writing the project (RAVEN architecture) documentation",
+      "Helped in creating the presentation's slideshow",
     ],
-    image: "emam.jpg",
-    qoute:
-      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
-  },
-  {
-    name: "Abdullah Badran",
-    responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
-    ],
-    image: "emam.jpg",
-    qoute:
-      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
-  },
-  {
-    name: "AbdelAziz Hosny",
-    responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
-    ],
-    image: "emam.jpg",
+    image: "andrew.jpeg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
   {
     name: "Mohamed Essam",
     responsibilities: [
-      "Helped in creating the front-end of the web application",
-      "Helped in creating the back-end of the web application",
-      "Helped in implementing the MulT model archeticture",
-      "Helped in feature extraction for the models' archetictures",
+      "Helped in implementing the MARNN & MMUUBA models' archetictures",
+      "Helped in feature extraction for the MARNN & MMUUBA models' archetictures",
+      "Helped in writing the project (MARNN & MMUUBA) documentation",
+      "Helped in creating the presentation's slideshow",
     ],
-    image: "emam.jpg",
+    image: "mohamedEssam.jpeg",
+    qoute:
+      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
+  },
+  {
+    name: "Abdullah Badran",
+    responsibilities: [
+      "Helped in implementing the MARNN & MMUUBA models' archetictures",
+      "Helped in creating the back-end of the web application",
+      "Helped in writing the project (Back-end) documentation",
+      "Helped in creating the presentation's slideshow",
+    ],
+    image: "noimage.jpg",
+    qoute:
+      "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
+  },
+  {
+    name: "AbdelAziz Hosny",
+    responsibilities: [
+      "Helped in implementing the MulT model archeticture",
+      "Helped in feature extraction for the MulT model archeticture",
+      "Helped in writing the project (MulT) documentation",
+      "Helped in creating the presentation's slideshow",
+    ],
+    image: "noimage.jpg",
     qoute:
       "I loved working with all my team members, or let me say 'my second family'. It was a long yet great journey for us all. I learned alot and I hope working with them again.",
   },
--- Front-end/src/pages/PredictionPage/PredictionPage.js
+++ Front-end/src/pages/PredictionPage/PredictionPage.js
@@ -5,6 +5,7 @@ import classes from "./PredictionPage.module.css";
 import axios from "axios";
 import Loader from "../../hoc/Loader/Loader";
 import { useSelector } from "react-redux";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const PredictionPage = (props) => {
   const { id } = useParams();
@@ -12,6 +13,10 @@ const PredictionPage = (props) => {
   const [showLoader, setShowLoader] = useState(true);
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [showVideo, setShowVideo] = useState(false);
+  const [uploadVideoPage, setUploadVideoPage] = useState(false);
+  const [predictClicked, setPredictClicked] = useState(false);
+  const [predictionsPage, setPredictionsPage] = useState(true);
+  const [prediction, setPrediction] = useState(70);
 
   const inputRef = useRef();
   const videoRef = useRef();
@@ -51,20 +56,34 @@ const PredictionPage = (props) => {
   };
 
   const predictClickHandler = () => {
-    const modelArch = model.arch;
-    const videoFormData = new FormData();
-    videoFormData.append("video", selectedVideo);
-    videoFormData.append("arch", modelArch);
-    axios
-      .post(`/models/predict/${model.id}`, videoFormData)
-      .then((res) => {
-        if (res.data.success) {
-          console.log(res.data.msg);
-        } else {
-          console.log(res.data.msg);
-        }
-      })
-      .catch((err) => console.log(err));
+    setPredictClicked(true);
+    setUploadVideoPage(false);
+    setTimeout(() => {
+      setPredictClicked(false);
+      setPredictionsPage(true);
+    }, 5000);
+    // const modelArch = model.arch;
+    // const videoFormData = new FormData();
+    // videoFormData.append("video", selectedVideo);
+    // videoFormData.append("arch", modelArch);
+    // axios
+    //   .post(`/models/predict/${model.id}`, videoFormData)
+    //   .then((res) => {
+    //     if (res.data.success) {
+    //       console.log(res.data.msg);
+    //     } else {
+    //       console.log(res.data.msg);
+    //     }
+    //   })
+    //   .catch((err) => console.log(err));
+    setSelectedVideo(false);
+    setShowVideo(false);
+  };
+
+  const returnToVideoHandler = () => {
+    setPredictClicked(false);
+    setPredictionsPage(false);
+    setUploadVideoPage(true);
   };
   return (
     <div className={classes.PredictionPage}>
@@ -75,26 +94,43 @@ const PredictionPage = (props) => {
         <div className={classes.container}>
           <h1 className={classes.header}>Predict the sentiment using {model.name} model</h1>
           <div className={classes.dash}></div>
-          <div className={[classes.body, selectedVideo ? classes.clearPadding : null].join(" ")}>
-            {showVideo ? (
-              <div className={classes.videoContainer}>
-                <video ref={videoRef} src="" controls></video>
+          {uploadVideoPage ? (
+            <div className={[classes.body, selectedVideo ? classes.clearPadding : null].join(" ")}>
+              {showVideo ? (
+                <div className={classes.videoContainer}>
+                  <video ref={videoRef} src="" controls></video>
+                </div>
+              ) : null}
+              <div className={classes.videoUploaderInput}>
+                <p>
+                  {screen !== "Mobile" ? <i className="fas fa-cloud-upload-alt"></i> : null} {!selectedVideo ? "Upload video" : "Upload another video"}
+                </p>
+                <input className={selectedVideo ? classes.marginInput : null} ref={inputRef} accept="video/mp4" type="file" onChange={videoSelectHandler} />
               </div>
-            ) : null}
-            <div className={classes.videoUploaderInput}>
-              <p>
-                {screen !== "Mobile" ? <i className="fas fa-cloud-upload-alt"></i> : null} {!selectedVideo ? "Upload video" : "Upload another video"}
-              </p>
-              <input className={selectedVideo ? classes.marginInput : null} ref={inputRef} accept="video/mp4" type="file" onChange={videoSelectHandler} />
+              {showVideo ? (
+                <div className={classes.predictBtnContainer}>
+                  <button className={classes.predictBtn} onClick={predictClickHandler}>
+                    Predict
+                  </button>
+                </div>
+              ) : null}
             </div>
-            {showVideo ? (
+          ) : predictClicked ? (
+            <div className={[classes.body, classes.predictingLoaderBody].join(" ")}>
+              <img src="/images/predicting_loader.gif" alt="loader" />
+              <p>Please wait until the model finishes predicting...</p>
+            </div>
+          ) : predictionsPage ? (
+            <div className={[classes.body, classes.predictionsPageBody].join(" ")}>
+              <ProgressBar className={classes.progressbar} completed={prediction} bgColor="#4f97c7" labelColor="#1a3f66" />
+              <p>Your video is {prediction}% positive</p>
               <div className={classes.predictBtnContainer}>
-                <button className={classes.predictBtn} onClick={predictClickHandler}>
-                  Predict
+                <button className={classes.predictBtn} onClick={returnToVideoHandler}>
+                  Predict on another video
                 </button>
               </div>
-            ) : null}
-          </div>
+            </div>
+          ) : null}
         </div>
       )}
     </div>
